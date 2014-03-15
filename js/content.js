b7d46ed914c283205a941ffb4de866f813173e8f
Michal Radecki <michalradecki@MacBook-Air-Michal.local>
2014-03-15 16:14:46 +0100

    zm

--- js/content.js
+++ js/content.js
@@ -1,3 +1,5 @@
+var pluginUrl = 'chrome-extension://' + chrome.i18n.getMessage("@@extension_id");
+
 function ContentController()
 {
 	var that = this;
@@ -10,7 +12,30 @@ function ContentController()
 	});
 }
 
-ContentController.prototype.showPlugin = function(){alert('show');}
-ContentController.prototype.hidePlugin = function(){alert('hide');}
+ContentController.prototype.showPlugin = function() {
+	if($('#bulletsplugin').length == 0)
+	{
+		$.get(pluginUrl + '/res/html/iframe.html', function(data){
+			var data = $(data);
+			$('body').append(data);
+
+			$('#bulletsplugin').attr('src', pluginUrl + '/res/html/plugin.html'); 
+			// $('.not_logged_in .logo img', $('#bulletsplugin').get(0).contentWindow).attr('src', pluginUrl + '/res/gfx/LogoLong.png'); 
+			
+			$('*', $('#bulletsplugin').get(0).contentWindow).remove();
+
+			$('#bulletsplugin').load(function(){
+				$('#bulletsplugin').animate({top: 0}, 250);
+			});
+		});
+	}
+	else {
+		$('#bulletsplugin').animate({top: 0}, 250);
+	}
+}
+
+ContentController.prototype.hidePlugin = function(){
+	$('#bulletsplugin').animate({top: -50}, 250);
+}
 
 var contentController = new ContentController();
