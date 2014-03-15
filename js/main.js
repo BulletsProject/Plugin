7d671215ae7fab1cc9e8705effc604238c8a230b
Michal Radecki <michalradecki@MacBook-Air-Michal.local>
2014-03-15 09:11:34 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// console.log('asd');
+// var bg = chrome.runtime.getBackgroundPage();
+// console.log('asd');
+// console.log(test);
+// console.log();
+
+alert('asd2');
+
+// function MainController()
+// {
+// }
+
+// MainController.prototype.run = function()
+// {
+// }
